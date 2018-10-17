952c9e25a992ea5f29cb5239050b596386528b44
Brian Stumbaugh <brian.stumbaugh@gmail.com>
2018-10-17 00:30:22 -0500

    initial

---
+++ frontend/pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Home = props => (
+    <div>
+        <p>HEyyyy</p>
+        <Link>
+            <a href="/sell">SELL YO</a>
+        </Link>
+    </div>
+)
+
+export default Home;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Home = props => (
+    <div>
+        <p>SELLLLLL</p>
+        <Link>
+            <a href="/">Homey</a>
+        </Link>
+    </div>
+)
+
+export default Home;
